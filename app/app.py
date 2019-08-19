3db35e7e128aeffa27342d9fd9e622efa91b4aa3
jascharf10 <jas1533@stern.nyu.edu>
2019-08-19 17:32:28 -0400

    created a flask app

--- app/app.py
+++ app/app.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def index():
+@app.route("/hello")
+def hello():
     return "Hello, World!"
 
+@app.route("/<int:value>")
+def home(value):
+    add_one = value + 1
+    return render_template('index.html', string="TESTING!!!", value=add_one)
+    
 if __name__ == "__main__":
     app.run(debug=True)
     
