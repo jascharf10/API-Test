a447d2798ea313409f9cf85dbad2754e6be22b1d
jascharf10 <jas1533@stern.nyu.edu>
2019-08-27 18:30:24 -0400

    more fun with jquery

--- app/static/main.js
+++ app/static/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     console.log("ready!");
+    $('#try-again').hide();
 
   $("form").on("submit", function() {
       console.log("the form has been submitted")
@@ -13,15 +14,28 @@ $(document).ready(function() {
         url: "/",
         data: {first: valueOne, second: valueTwo},
         success: function(results) {
-            console.log(results.items[0]);
-            $("#results").html('<a href="'+results.items[0].html_url+'">'+results.items[0].login+'</a><br><img src="'+results.items[0].avatar_url+'">')
-            $("input").val("")
+          if (results.items.length > 0) {
+              $('input').hide();
+              $('#try-again').show();
+            var randNum = Math.floor(Math.random() * Object.keys(results.items).length)
+            console.log(results.items[randNum]);
+            $("#results").html('<a href="'+results.items[randNum].html_url+'">'+results.items[randNum].login+'</a><br><img src="'+results.items[randNum].avatar_url+'" class="avatar">')
+            // $("input").val("")
+          } else {
+              $('#results').html('Something went terribly wrong! Please try again.')
+          }
         },
         error: function(error) {
             console.log(error);
         }
     });
       
+  });
+
+  $('#try-again').on('click', function(){
+      $('input').val('').show();
+      $('#try-again').hide();
+      $('#results').html('');
   });
     
 });
