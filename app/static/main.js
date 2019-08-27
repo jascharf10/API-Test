ae33cc10113fd62d506db712b1eae75bc45a4541
jascharf10 <jas1533@stern.nyu.edu>
2019-08-27 11:40:29 -0400

    added github api

--- app/static/main.js
+++ app/static/main.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
   $("form").on("submit", function() {
       console.log("the form has been submitted")
       
-    var valueOne = $('input[name="number-one"]').val()
-    var valueTwo = $('input[name="number-two"]').val()
+    var valueOne = $('input[name="location"]').val()
+    var valueTwo = $('input[name="language"]').val()
       console.log(valueOne, valueTwo)
  
     $.ajax({
@@ -13,8 +13,8 @@ $(document).ready(function() {
         url: "/",
         data: {first: valueOne, second: valueTwo},
         success: function(results) {
-            console.log(results);
-            $("#results").html(results.total)
+            console.log(results.items[0]);
+            $("#results").html('<a href="'+results.items[0].html_url+'">'+results.items[0].login+'</a><br><img src="'+results.items[0].avatar_url+'">')
             $("input").val("")
         },
         error: function(error) {
